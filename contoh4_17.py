a6a93dbd0d18cadb491b7c8e8cd279e73175e832
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-10-26 11:19:24 +0800

    Update contoh4_17.py 1

--- contoh4_17.py
+++ contoh4_17.py
@@ -1,24 +1,26 @@
 # function mengira purata
-def kira_purata(jumlah, bilangan):
+def kira_purata(x, y):
     purata = jumlah / bilangan
     return purata
 
 # atur cara utama
 def main():
-    senaraiNo = [] # menggunakan list[10, 20, 30, 40] (sebagai contoh)
-    cont = True # Boolean ; cont = continue
-    while cont: # struktur kawalan ulangan "selagi"
-        try: # cuba dan tangkap value error
+    senaraiNo = []
+    cont = True
+    while cont:
+        try:
             nom = float(input("Masukkan nombor [X utk berhenti]: "))
             senaraiNo.append(nom)
         except ValueError:
             cont = False
 
-    bilangan = len(senaraiNo) # kira bilangan nombor yang dimasukkan; len( ) = built-in function
-    if bilangan > 0: # Pembetulan ralat di baris ini
-        jumlah = sum(senaraiNo) # jumlahkan semua nombor; sum( ) = built-in function
-        print(f"Purata = {kira_purata(jumlah, bilangan)}")
+    bilangan = len(senaraiNo)
+    
+    jumlah = sum(senaraiNo)
+    print(f"Purata = {kira_purata(jumlah, bilangan)}")
     print("Tamat")
+
+# JANGAN ubah kod di bawah!
 
 if __name__ == "__main__":
     main()
